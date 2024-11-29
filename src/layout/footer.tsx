d6d948841a5b4977116616fbf2e29fe2f49b937e
Mohammed R. M <m.mohammed@geniodiligence.it>
2024-11-29 11:45:54 +0100

    add footer and contact info

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -1,5 +1,13 @@
-import React from "react";
-
 export default function Footer() {
-  return <footer></footer>;
+  return (
+    <footer className="h-10 place-items-center flex justify-around">
+      Designed By Mohammed Ridha M. M.
+      <a className="text-lg text-blue-300 font-bold" href="https://www.linkedin.com/in/mohammed-mohammed-515932100/">
+        LinkedIn
+      </a>
+      <a className="text-lg text-blue-300 font-bold" href="https://www.facebook.com/mohammed.redha.98">
+        Facebook
+      </a>
+    </footer>
+  );
 }
--- src/layout/template.tsx
+++ src/layout/template.tsx
@@ -1,3 +1,4 @@
+import Footer from './footer';
 import Headers from './header';
 import Sidebar from './sidebar';
 
@@ -11,6 +12,9 @@ export default function Template({ children }: { children: JSX.Element | JSX.Ele
       <main className="w-full md:h-screen flex mt-8  bg-white">
         <div className="  w-full">{children}</div>
       </main>
+      <div className={`fixed z-50 shadow-md text-slate-50 bg-gray-900 w-full bottom-0`}>
+        <Footer />
+      </div>
     </div>
   );
 }
